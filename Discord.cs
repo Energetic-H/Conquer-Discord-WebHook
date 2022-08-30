b3e08d16cd4f955b032c13f02e6d42eb91843e7a
Enrergetic(H) <112508146+Energetic-H@users.noreply.github.com>
2022-08-30 19:45:17 +0200

    Add files via upload

---
+++ Discord.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Specialized;
+using System;
+using System.Net.Http;
+using System.Net;
+
+namespace COServer
+{
+    class Discord
+    {
+        public static void DiscordSendMessage(string username, string content, string face)
+        {
+            if (content.Contains("@everyone") || content.Contains("@"))
+            {
+                return;
+            }
+            WebClient wc = new WebClient();
+            try
+            {
+                wc.UploadValues("https://discord.com/api/webhooks/994871482188578858/6qBi5ldbSsRXB5HWf2jJV_jfVKYKZ657YQ7k626mBlpDPCmLi_iSDD2xVbxPl4KOhVAB", new NameValueCollection
+            {
+                {
+                    "content",content
+                },
+                {
+                    "username",username
+                },
+                {
+                    "avatar_url",face
+                }
+            });
+               
+            }
+            catch (WebException ex)
+            {
+               
+            }
+          
+
+        
+        }
+    }
+}
+        
+ 
+
+
